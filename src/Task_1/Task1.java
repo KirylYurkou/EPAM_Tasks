5f347303a116755a0ca9a3680aaea2b4ea026f17
KirylYurkou <67512589+KirylYurkou@users.noreply.github.com>
2020-07-08 10:24:44 +0300

    1 finished

--- src/Task_1/Task1.java
+++ src/Task_1/Task1.java
@@ -1,5 +1,22 @@
 package Task_1;
 
+import java.util.Scanner;
+
 public class Task1 {
-    
+
+    //1. Найдите значение функции: z = ((a – 3 )*b/2) + c.
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double a, b, c;
+        System.out.println("Введите значение a");
+        a = scanner.nextDouble();
+        System.out.println("Введите значение b");
+        b = scanner.nextDouble();
+        System.out.println("Введите значение c");
+        c = scanner.nextDouble();
+        double z = ((a - 3) * b / 2) + c;
+        System.out.println("Результат = " + z);
+        scanner.close();
+    }
 }
