1a2181552a4977b2ab9caa2f666cc1a8c9a72ab7
KirylYurkou <67512589+KirylYurkou@users.noreply.github.com>
2020-07-08 11:24:49 +0300

    finished

--- src/Task_1/Task2.java
+++ src/Task_1/Task2.java
@@ -1,18 +1,21 @@
 package Task_1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 
 public class Task2 {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         double a, b, c;
         System.out.println("Введите значение a");
-        a = scanner.nextDouble();
+        a = Double.parseDouble(reader.readLine());
         System.out.println("Введите значение b");
-        b = scanner.nextDouble();
+        b = Double.parseDouble(reader.readLine());
         System.out.println("Введите значение c");
-        c = scanner.nextDouble();
+        c = Double.parseDouble(reader.readLine());
         double z = (b+(Math.sqrt(Math.pow(b,2))+4*a*c))/2*a-(Math.pow(a,3)*c)+Math.pow(b,-2);
         System.out.println("Результат = "+z);
     }
