46b6d298d0862da5bd43ee4fd53362a7f62ada8d
KirylYurkou <67512589+KirylYurkou@users.noreply.github.com>
2020-07-08 16:39:46 +0300

    finished

---
+++ src/Task_1/Task4.java
@@ -0,0 +1,18 @@
+package Task_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Дано действительное число Rвида nnn.ddd(три цифровых разряда в дробной и целой частях).
+// Поменять местами дробную и целую части числа и вывести полученное значение числа.
+public class Task4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число с тремя знаками до запятой и тремя после");
+        double x = Double.parseDouble(reader.readLine());
+        double y = (x * 1000) % 1000 + (int) x / 1000.0;
+        System.out.println(y);
+        reader.close();
+    }
+}
