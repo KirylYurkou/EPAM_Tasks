db6bd094c55769682cc60e89b95be70df86b1f31
KirylYurkou <67512589+KirylYurkou@users.noreply.github.com>
2020-07-12 14:47:47 +0300

    finished

---
+++ src/Task2_Branchesc/Task5.java
@@ -0,0 +1,23 @@
+package Task2_Branchesc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Вычислить значение функции:
+public class Task5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите x");
+        double x = Double.parseDouble(reader.readLine());
+        double a,b;
+        if (x<=3){
+            a = Math.pow(x,2)-3*x+9;
+            System.out.println("F(x) равен "+a);
+        }else if (x>3){
+            b = 1/(Math.pow(x,3)+6);
+            System.out.println("F(x) равен "+b);
+        }
+        reader.close();
+    }
+}
